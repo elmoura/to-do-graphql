7b8742f6af394675381a52908e6e6a832efd60de
Gabriel Moura <mourasb.gabriel@gmail.com>
2021-11-29 14:55:41 -0300

    refac: remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ToDoModule } from '@use-cases/to-do/to-do.module';
+import { DatabaseConnectionModule } from '@config/db-connection.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ToDoModule,
+    DatabaseConnectionModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/config/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
 })
 export class AppModule {}
